acf1d31d4c2bc2e8f811296f8bb2adb5599e2487
adnan <adnan@hamyaraval.site>
2019-06-24 11:22:25 +0430

    customize time blocking and handle exception

--- workers/workers/Worker.php
+++ workers/workers/Worker.php
@@ -5,32 +5,37 @@ namespace Workers;
 use Workers\Abstracts\AbstractWorker;
 use Workers\Extras\Logger;
 use Workers\Extras\Timer;
+use Workers\Exceptions\WorkerTimeOutException;
 
 class Worker extends AbstractWorker {
     public function channel() {
         parent::channel();
         return $this->callback(function ($msg) {
             $callback = $this->getJobName($msg);
-            try {
-                if (getArgv('time') === 'block') {
-                    $wait = (time() - (strtotime(getArgv('start') . ' +1 day')));
 
-                    if ($wait < 0) {
-                        sleep($wait * -1);
-                    }
-                }
+            try {
+                $this->checkBlock();
 
                 if (! (new Timer())->check()) {
                     $this->ack($msg);
-                    throw new \Exception('UnTime Task Exception');
+                    throw new WorkerTimeOutException('Time out Task Exception');
                 }
 
                 (new $callback)($msg);
+            } catch (WorkerTimeOutException $e) {
+                Logger::alert(
+                    Job::getJobName($msg),
+                    json_encode(Job::getJobData($msg)),
+                    json_encode([]),
+                    json_encode([
+                        'message' => $e->getMessage() ? $e->getMessage() : 'NULL',
+                        'code' => $e->getCode() ? $e->getCode() : 'NULL',
+                    ]));
             } catch (\Throwable $e) {
                 Logger::alert(
                     Job::getJobName($msg),
                     json_encode(Job::getJobData($msg)),
-                    [],
+                    json_encode([]),
                     json_encode([
                         'message' => $e->getMessage() ? $e->getMessage() : 'NULL',
                         'file' => $e->getFile() ? $e->getFile() : 'NULL',
