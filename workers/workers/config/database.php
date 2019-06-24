f0bedd5b3e448ee16d5ddf06323b23a2d8e9eb56
adnan <adnan@hamyaraval.site>
2019-06-24 11:26:48 +0430

    add driver field to specify connection type

--- workers/workers/config/database.php
+++ workers/workers/config/database.php
@@ -1,17 +1,22 @@
 <?php
 
 return [
-    'db' => [
-        'type' => env('DB_TYPE', 'mongodb'),
-    ],
     'mongo'         => [
+        /**
+         * drivers are [mongodb, pgsql, mysql, odbc]
+         */
+        'driver' => env('MONGO_DRIVER', 'mongodb'),
         'db' => env('MONGO_DB', 'test'),
         'user' => env('MONGO_USER', 'root'),
         'pass' => env('MONGO_PASS', 'secret'),
         'host' => env('MONGO_HOST', 'mongo'),
         'port' => env('MONGO_PORT', 27017),
     ],
-    'mongo2'         => [
+    'hamyar'         => [
+        /**
+         * drivers are [mongodb, pgsql, mysql, odbc]
+         */
+        'driver' => env('MONGO2_DRIVER', 'mongodb'),
         'db' => env('MONGO2_DB', 'test'),
         'user' => env('MONGO2_USER', 'root'),
         'pass' => env('MONGO2_PASS', 'secret'),
