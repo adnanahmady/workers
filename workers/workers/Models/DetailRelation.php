49a51bd74168f2a4f606ab02a65be112b5a2fada
adnan <adnan@hamyaraval.site>
2019-06-26 17:51:45 +0430

    add getUser method

--- workers/workers/Models/DetailRelation.php
+++ workers/workers/Models/DetailRelation.php
@@ -7,4 +7,21 @@ use Worker\Core\Model;
 class DetailRelation extends Model
 {
     protected $connection = 'hamyar';
+
+    /**
+     * Get drivers wallet amount
+     *
+     * @param $args
+     * @return int
+     */
+    public function getUser($filter)
+    {
+        $filter = (is_array($filter) ? $filter : ['detail_code' => (string) $filter]);
+        $options = [
+            'projection' => ['userid' => 1]
+        ];
+        $res = static::find($filter, $options)->toArray();
+
+        return (!empty($res) && isset($res[0]['userid'])) ? $res[0]['userid'] : 0;
+    }
 }
