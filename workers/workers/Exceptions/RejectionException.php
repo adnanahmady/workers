fec5486bd186faa92b9febeacb3a2c85b2375aef
adnan <adnan@hamyaraval.site>
2019-08-19 16:35:46 +0430

    create rejection exception handler

---
+++ workers/workers/Exceptions/RejectionException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Worker\Exceptions;
+
+use Throwable;
+
+class RejectionException extends \Exception {
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
